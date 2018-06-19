c6c9c0113332445e8d85c4695aded95cc6472904
許家翔 <sjshiang@hotmail.com>
2018-06-19 23:57:34 +0800

    " 新增最簡單的物件半徑範例_物件導向與原型（運用this） "

--- NodeJS_習題/10_circle.js
+++ NodeJS_習題/10_circle.js
@@ -1,7 +1,7 @@
 var circle = {
-  r: 3,
-  area: function () {
-    return 3.14 * this.r * this.r;
+  r: 3, //半徑是3
+  area: function () { //定義 area 函數
+    return 3.14 * this.r * this.r; //所謂的 this 就是指向現在的單一物件 circle
   }
 }
 console.log("circle.r=%d", circle.r);
