0dbb7b1a79a1fb27879cb3f609233241af9d9c0e
許家翔 <sjshiang@hotmail.com>
2018-07-16 09:52:06 +0800

    " 新增反矩陣範例(用 npm 安裝外部套件、require指令介紹) "

--- NodeJS_Exercise/19_numericTest.js
+++ NodeJS_Exercise/19_numericTest.js
@@ -14,7 +14,10 @@
 
 
 
-// import numeric from "numeric"; 原本宣告下面那行系統自動提示 ES6 寫法
+/* import numeric from "numeric"; 原本宣告下面那行系統自動提示 ES6 寫法
+   可以使用 require 這個指令動態的引入該模組，注意 require 必須採用相對路徑，
+   即使在同一個資料夾底下，也要加上 ./ 的前置符號，代表在目前資料夾之下。
+*/
 
 
 var numeric = require("numeric");
