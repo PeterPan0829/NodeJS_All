bbd2893a3cea48db9daf04bd6b12c30120e5d626
許家翔 <sjshiang@hotmail.com>
2018-07-17 16:54:59 +0800

    " 新增引用物件模組：匯出建構函數(透過建構函數 cir() 建立物件, 第二種寫法) "

--- NodeJS_Exercise/Module_Example/08_CircleTest.js
+++ NodeJS_Exercise/Module_Example/08_CircleTest.js
@@ -7,3 +7,11 @@ var cir = require('./07_circle'); // 注意 ./ 代表 circle 與此程式放在�
 
 var c = new cir(5);
 console.log("c.area()=" + c.area());
+
+
+/*
+現在應該可以理解為何要將 Circle 定義為一個函數了吧！
+這只不過 Circle 類別的建構函數而已，當他被 module.exports = Circle 這個指令匯出時，
+就可以在 var cir = require('./circle') 這個指令接收到建構函數，
+然後再利用像 var c = new cir(5) 這樣的指令，呼叫該建構函數以建立出物件。
+*/
